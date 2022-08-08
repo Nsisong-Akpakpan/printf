fa0030d24712dab1fe87e1eb002f5fdc5dc6e358
WamuyuMunene <wamuyumunene98@gmail.com>
2022-08-08 10:40:37 -0700

    string_rev

--- string_rev.c
+++ string_rev.c
@@ -1,50 +1,25 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
 
-/*tr_reverse - reverse a string
- * @ap: arg list
- *  * Return: number printed char
+/**
+ *  * printf_srev - function that prints a str in reverse
+ *   * @args: type struct va_arg where is allocated printf arguments
+ *    *
+ *     * Return: the string
 */
- int print_str_reverse(va_list ap)
+int printf_srev(va_list args)
 {
-	char *argument = va_arg(ap, char *), *str;
-	int size, left, limit, right, sum = 0;
 
-	if (!argument)
-	{
-	sum += _puts("%r", 0);
-	return (sum);
-	}
+	char *s = va_arg(args, char*);
+	int i;
+	int j = 0;
 
-	size = _strlen_recursion(argument);
-	right = size - 1;
-	limit = (size % 2 == 0) ? (size + 1) / 2 : (size / 2);
-
-	str = malloc(sizeof(char) * size + 1);
-
-	if (str == NULL)
-	{
-	return (0);
-	}
-
-	if (size % 2 != 0)
-	{
-	str[limit] = argument[limit];
-	}
-
-	for (left = 0; left < limit; left++)
-	{
-	str[left] = argument[right];
-	str[right] = argument[left];
-	right--;
-	}
-
-	str[size] = '\0';
-
-	sum = _puts(str, 0);
-	free(str);
-
-	return (sum);
+	if (s == NULL)
+	s = "(null)";
+	while (s[j] != '\0')
+	j++;
+	for (i = j - 1; i >= 0; i--)
+	_putchar(s[i]);
+	return (j);
 }
